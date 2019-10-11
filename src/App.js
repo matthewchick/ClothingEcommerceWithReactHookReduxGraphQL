aeaf0e666c877897d7bf2f91ad82fc205e7475b5
Matthew Chick <c.chick@unsw.edu.au>
2019-10-11 13:28:46 +1100

    implement userReducer and userAction to header and app components

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
 import { Route, Switch } from 'react-router-dom';
 import './App.css';
 import HomePage from './pages/homepage/homepage.component';
@@ -6,39 +6,47 @@ import ShopPage from './pages/shop/shop.component';
 import Header from './components/header/header.component';
 import SignInAndSignUpPage from './pages/sign-in-out/sign-in-out.component';
 import { auth , createUserProfileDocument } from './firebase/firebase.utils';
+import { connect } from 'react-redux';
+import { setCurrentUser } from './redux/user/user.actions';
+
 class App extends Component{
-  constructor() {
+  /* constructor() { //use redux, the following are not needed
     super();
     this.state = {
       currentUser: null
     }
-  }
+  } */
   unsubsribeFromAuth = null
   componentDidMount() {
-    auth.onAuthStateChanged(async userAuth => {
+    const { setCurrentUser } = this.props;
+    this.unsubsribeFromAuth = auth.onAuthStateChanged(async userAuth => {
       if (userAuth){
         const userRef = await createUserProfileDocument(userAuth);
         //if storing google user authentication, 
         userRef.onSnapshot(snapShot => {
           console.log('Get Firebase',snapShot.data());  //snapShot.data() includes createdAt, displayName and email
-          this.setState({
+            setCurrentUser({
+              id: snapShot.id,
+              ...snapShot.data()
+            });
+          }); 
+          /*this.setState({
             currentUser: {
               id: snapShot.id,
               ...snapShot.data()
             }
           }, () => {
             console.log('this.state', this.state);
-          })
-        })     
-      }
-      this.setState({currentUser: userAuth})
+            }) 
+          }) */  
+        }
+        setCurrentUser(userAuth);
     })
   }
   componentWillUnmount() {
     this.unsubsribeFromAuth();
   }
-  render() {
-    // remove <Header currentUser={this.state.currentUser}/> when using redux
+  render() { // remove <Header currentUser={this.state.currentUser}/> when using redux
     return (
       <div>
         <Header />
@@ -51,4 +59,7 @@ class App extends Component{
     );
   }
 }
-export default App;
+const mapDispatchToProps = dispatch => ({  // https://react-redux.js.org/using-react-redux/connect-mapdispatch
+  setCurrentUser: user => dispatch(setCurrentUser(user))
+});
+export default connect(null, mapDispatchToProps)(App);
