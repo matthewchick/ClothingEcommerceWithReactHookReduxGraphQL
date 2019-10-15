b481097db5e34facb15cc655eae5c57d515de043
Matthew Chick <c.chick@unsw.edu.au>
2019-10-15 12:18:20 +1100

    add cart item into cart reducer

--- src/components/collection-item/collection-item.component.jsx
+++ src/components/collection-item/collection-item.component.jsx
@@ -1,9 +1,13 @@
 import React from 'react';
 import CustomButton from '../custom-button/custom-button.component';
+import { connect } from 'react-redux';
+import { addItem } from '../../redux/cart/cart.actions';
 
 import './collection-item.styles.scss';
 
-const CollectionItem=({ id, name, price, imageUrl}) => (
+const CollectionItem=({ item, addItem }) => {
+    const { name, price, imageUrl } = item;
+    return (
     <div className='collection-item'>
         <div className='image'
         style={{
@@ -14,8 +18,12 @@ const CollectionItem=({ id, name, price, imageUrl}) => (
             <span className='name'>{name}</span>
             <span className='price'>{price}</span>
         </div>
-        <CustomButton inverted>Add to cart</CustomButton>
+        <CustomButton onClick={() => addItem(item)} inverted>Add to cart</CustomButton>
     </div>
-)
+)};
 
-export default CollectionItem;
+const mapDispatchToProps = dispatch => ({  
+    addItem: item => dispatch(addItem(item))  //call addItem() in cart.action
+}); 
+
+export default connect(null, mapDispatchToProps)(CollectionItem);
--- src/redux/cart/cart.reducer.js
+++ src/redux/cart/cart.reducer.js
@@ -1,7 +1,8 @@
 import { CartActionTypes } from './cart.types';
 
 const INITIAL_STATE = {
-    hidden: true    //empty currentUser
+    hidden: true,    // check cart button whether it is clicked
+    cartItems: []
 }
 
 const cartReducer = (state = INITIAL_STATE, action) => {
@@ -10,6 +11,11 @@ const cartReducer = (state = INITIAL_STATE, action) => {
             return {           //return an object
                 ...state,      //create a new state
                 hidden: !state.hidden
+            };
+        case CartActionTypes.ADD_ITEM:
+            return {
+                ...state,
+                cartItems: [...state.cartItems, action.payload]
             }
         default:  //don't care the action
             return state;
