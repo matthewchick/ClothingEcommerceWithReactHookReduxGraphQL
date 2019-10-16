9eee14f45ace29c8e30e2038e2ee1a6f8bd86334
Matthew Chick <c.chick@unsw.edu.au>
2019-10-17 09:20:47 +1100

    how to use dispatch to pass action to CustomButton when checkout

--- src/components/cart-dropdown/cart-dropdown.component.jsx
+++ src/components/cart-dropdown/cart-dropdown.component.jsx
@@ -4,10 +4,12 @@ import CustomButton from '../custom-button/custom-button.component';
 import CartItem from '../cart-item/cart-item.component';
 import { createStructuredSelector } from 'reselect';
 import { selectCartItems } from '../../redux/cart/cart.selectors';
-import './cart-dropdown.styles.scss';
 import { withRouter } from 'react-router-dom';
+import { toggleCartHidden } from '../../redux/cart/cart.actions';
 
-const CartDropdown = ({ cartItems, history }) => (
+import './cart-dropdown.styles.scss';
+
+const CartDropdown = ({ cartItems, history, dispatch }) => (
     <div className='cart-dropdown'>
         <div className='cart-items'/>
          {
@@ -19,7 +21,12 @@ const CartDropdown = ({ cartItems, history }) => (
              <span className='empty-message'>Your cart is empty</span>
              )
          }
-        <CustomButton onClick={()=>history.push('/checkout')}>GO TO CHECKOUT</CustomButton>
+        <CustomButton onClick={()=> {
+            history.push('/checkout');
+            dispatch(toggleCartHidden());   //pass action with dispatch
+        }}>
+            GO TO CHECKOUT
+        </CustomButton>
     </div>
 );
 // change the state without rendering to enhance performing
