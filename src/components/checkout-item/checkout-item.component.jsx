d600d25856d52fb627c04bde396baed3337b0f94
Matthew Chick <c.chick@unsw.edu.au>
2019-10-17 10:54:36 +1100

    increase and decrease quantity

--- src/components/checkout-item/checkout-item.component.jsx
+++ src/components/checkout-item/checkout-item.component.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { connect } from 'react-redux';
-import { clearItemFromCart } from '../../redux/cart/cart.actions';
+import { clearItemFromCart, addItem, removeItem } from '../../redux/cart/cart.actions';
 import { createStructuredSelector } from 'reselect';
 import './checkout-item.styles.scss';
 
@@ -10,7 +10,7 @@ const CheckoutItem = ({ cartItem: { name, imageUrl, price, quantity }}) => (
 const CheckoutItem = ({ cartItem, clearItem }) => (
     { name, imageUrl, price, quantity } = cartItem;
 */
-const CheckoutItem = ({ cartItem, clearItem }) => {
+const CheckoutItem = ({ cartItem, clearItem, addItem, removeItem }) => {
     const { name, imageUrl, price, quantity } = cartItem;
     return(
     <div className='checkout-item'>
@@ -18,14 +18,20 @@ const CheckoutItem = ({ cartItem, clearItem }) => {
             <img src={ imageUrl } alt='item' />
         </div>
         <span className='name'>{name}</span>
-        <span className='quantity'>{quantity}</span>
-        <span className='price'>{price}</span>
+        <span className='quantity'>
+            <div className='arrow' onClick={() => removeItem(cartItem)}>&#10094;</div>
+                <span className='value'>{quantity}</span>
+            <div className='arrow' onClick={() => addItem(cartItem)}>&#10095;</div>
+        </span>
+        <span className='price'>${price}</span>
         <div className='remove-button' onClick={() => clearItem(cartItem)}>&#10005;</div>
     </div>
 )}
 
 const mapDispatchToProps = dispatch => ({  
-    clearItem: item => dispatch(clearItemFromCart(item))
+    clearItem: item => dispatch(clearItemFromCart(item)),
+    addItem: item => dispatch(addItem(item)),
+    removeItem: item => dispatch(removeItem(item))
 }); 
 
 export default connect (null, mapDispatchToProps)(CheckoutItem);
--- src/redux/cart/cart.actions.js
+++ src/redux/cart/cart.actions.js
@@ -11,6 +11,11 @@ export const addItem = item => ({
     payload: item
 });
 // remove item from Cart
+export const removeItem = item => ({
+    type: CartActionTypes.REMOVE_ITEM,
+    payload: item
+});
+// clear item from Cart
 export const clearItemFromCart = item => ({
     type: CartActionTypes.CLEAR_ITEM_FROM_CART,
     payload: item
