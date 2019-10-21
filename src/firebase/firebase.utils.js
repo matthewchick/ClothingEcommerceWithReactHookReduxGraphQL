928548a7fafb4b44e4157b7f166ac9fe887f1c25
Matthew Chick <c.chick@unsw.edu.au>
2019-10-21 14:05:48 +1100

    Retrieve shop data from firebase into app

--- src/firebase/firebase.utils.js
+++ src/firebase/firebase.utils.js
@@ -63,6 +63,19 @@ export const addCollectionAndDocuments = async(collectionKey, objectsToAdd) => {
     return await batch.commit()
 };
 
+export const convertCollectionsSnapshotToMap = (collections) => {
+    const transformedCollection = collections.docs.map(doc => {
+        const { title, items } = doc.data();
+        return {
+            routeName: encodeURI(title.toLowerCase()),
+            id: doc.id,
+            title,
+            items
+        };
+    });
+    console.log(transformedCollection);
+}
+
 firebase.initializeApp(config);
 export const auth = firebase.auth();
 export const firestore = firebase.firestore();
--- src/pages/shop/shop.component.jsx
+++ src/pages/shop/shop.component.jsx
@@ -1,14 +1,37 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { Route } from 'react-router-dom';
 
 import CollectionsOverview from '../../components/collections-overview/collections-overview.component';
 import CollectionPage from '../collection/collection.component'; 
+
+import { firestore, convertCollectionsSnapshotToMap } from '../../firebase/firebase.utils';
+
 /*
 When the router'path and location are successfully matched,
 a 'match' object is created. 
 This object contains information about the 'URL' and the 'path'
 It is used for nested route  like /shop/hats, /shop/jacket ...
 */
+class ShopPage extends Component {
+    unsubscribeFromSnapshot = null;
+
+    componentDidMount() {
+        const collectionRef = firestore.collection('collections');
+        collectionRef.onSnapshot(async snapshot => {
+            convertCollectionsSnapshotToMap(snapshot);
+        });
+    }
+    render() {
+        const { match } = this.props;
+        return (
+            <div className='shop-page'>
+            <Route exact path={`${match.path}`} component={CollectionsOverview} />
+            <Route path={`${match.path}/:collectionId`} component={CollectionPage} />
+            </div>
+        )  
+    }
+}
+/*
 const ShopPage = ({ match }) => {
     console.log('match', match);
     return (
@@ -18,7 +41,7 @@ const ShopPage = ({ match }) => {
         </div>
     )
 }
-
+*/
 export default ShopPage;
 /*
 class ShopPage extends Component {
